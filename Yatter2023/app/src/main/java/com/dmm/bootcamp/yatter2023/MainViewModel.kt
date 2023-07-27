9338e4ea2b63c58619995022ab681fe43fe31c8f
インターン用 <intern@intanyounoMacBook-Pro.local>
2023-07-27 11:28:58 +0900

    Yetter app

--- Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/MainViewModel.kt
+++ Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/MainViewModel.kt
@@ -1,8 +1,27 @@
 package com.dmm.bootcamp.yatter2023
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.dmm.bootcamp.yatter2023.domain.service.CheckLoginService
+import com.dmm.bootcamp.yatter2023.util.SingleLiveEvent
+import kotlinx.coroutines.launch
 
 class MainViewModel(
   private val checkLoginService: CheckLoginService,
-) : ViewModel()
+  private val _navigateToLogin: SingleLiveEvent<Unit> = SingleLiveEvent(),
+  val navigateToLogin: LiveData<Unit> = _navigateToLogin,
+  private val _navigateToPublicTimeline: SingleLiveEvent<Unit> = SingleLiveEvent(),
+  val navigateToPublicTimeline: LiveData<Unit> = _navigateToPublicTimeline
+) : ViewModel() {
+  fun onCreate() {
+    viewModelScope.launch {
+      if (checkLoginService.execute()) {
+        _navigateToPublicTimeline.value = Unit
+      } else {
+        _navigateToLogin.value = Unit
+      }
+    }
+  }
+
+}
--- Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/di/ViewModelModule.kt
+++ Yatter2023/app/src/main/java/com/dmm/bootcamp/yatter2023/di/ViewModelModule.kt
@@ -1,6 +1,9 @@
 package com.dmm.bootcamp.yatter2023.di
 
 import com.dmm.bootcamp.yatter2023.MainViewModel
+import com.dmm.bootcamp.yatter2023.ui.login.LoginViewModel
+import com.dmm.bootcamp.yatter2023.ui.post.PostViewModel
+import com.dmm.bootcamp.yatter2023.ui.timeline.PublicTimelineViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -8,6 +11,6 @@ internal val viewModelModule = module {
   viewModel { MainViewModel(get()) }
   viewModel { PublicTimelineViewModel(get()) }
   viewModel { PostViewModel(get(), get()) }
-  viewModel { RegisterAccountViewModel(get()) }
+//  viewModel { RegisterAccountViewModel(get()) }
   viewModel { LoginViewModel(get()) }
 }
